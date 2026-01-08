b55c19d5065f82e828f0adb8ccef2aba5ed21316
Tharindu-TPK <tharindupushpakumara59@gmail.com>
2026-01-08 18:34:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "hM8wjYYI#eKgPZvif8z3e5_a56d4kZanQmrlMOs23vKg27GLzhUY",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:uxFPCLlTDJpoAAfwHjiIbTFSlqOrbMhR@crossover.proxy.rlwy.net:33417",
+  OWNER_NUM: process.env.OWNER_NUM || "94787791638",
 };
